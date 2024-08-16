aff49f1b037f33c26d5ada1fb77996f05af693af
kaitorisenkou <kaitorisenkou@icloud.com>
2024-08-16 17:43:41 +0900

    Can cast while down succesfully

--- CastableInUnconscious/CastableInUnconscious.cs
+++ CastableInUnconscious/CastableInUnconscious.cs
@@ -24,7 +24,14 @@ namespace CastableInUnconscious {
                 new HarmonyMethod(typeof(CastableInUnconscious), nameof(Patch_GizmoDisabled), null),
                 null
                 );
-            
+            harmony.Patch(
+                AccessTools.Method(typeof(Job), nameof(Job.CanBeginNow), null, null),
+                null,
+                null,
+                new HarmonyMethod(typeof(CastableInUnconscious), nameof(Patch_CanBeginNow), null),
+                null
+                );
+
             Log.Message("[CastableInUnconscious] Harmony patch complete!");
         }
 
@@ -47,6 +54,33 @@ namespace CastableInUnconscious {
         }
         static bool HasModExt(Ability ability) {
             return ability.def.HasModExtension<ModExtension_CastableInUnconscious>();
+        }
+
+        static IEnumerable<CodeInstruction> Patch_CanBeginNow(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
+            var instructionList = instructions.ToList();
+            int patchCount = 0;
+            Label newLabel = generator.DefineLabel();
+            for (int i = 0; i < instructionList.Count; i++) {
+                if (instructionList[i].opcode == OpCodes.Starg_S) {
+                    CodeInstruction firstInst = new CodeInstruction(OpCodes.Ldarg_0);
+                    firstInst.labels = instructionList[i + 1].labels;
+                    instructionList[i + 1].labels = new List<Label>() { newLabel };
+                    instructionList.InsertRange(i + 1, new CodeInstruction[]{
+                        firstInst,
+                        new CodeInstruction(OpCodes.Call,AccessTools.Method(typeof(CastableInUnconscious),nameof(HasModExt_Job))),
+                        new CodeInstruction(OpCodes.Brfalse_S, newLabel),
+                        new CodeInstruction(OpCodes.Ldc_I4_0),
+                        new CodeInstruction(OpCodes.Starg_S, instructionList[i].operand),
+                    });
+                    patchCount++;
+                    break;
+                }
+            }
+            return instructionList;
+        }
+        static bool HasModExt_Job(Job job) {
+            var ability = job.ability;
+            return ability!=null && ability.def.HasModExtension<ModExtension_CastableInUnconscious>();
         }
     }
     public class ModExtension_CastableInUnconscious : DefModExtension {
